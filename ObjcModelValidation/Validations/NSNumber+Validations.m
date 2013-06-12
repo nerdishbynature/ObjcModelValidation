0c8364865b7aad0c4a5643cdc7d3473ca27d9925
Piet Brauer <piet@nerdishbynature.com>
2013-06-12 23:10:03 +0200

    [ADDED] validation category for NSNumber

---
+++ ObjcModelValidation/Validations/NSNumber+Validations.m
@@ -0,0 +1,13 @@
+//
+//  NSNumber+Validations.m
+//  ObjcModelValidation
+//
+//  Created by Piet Brauer on 12.06.13.
+//  Copyright (c) 2013 Piet Brauer. All rights reserved.
+//
+
+#import "NSNumber+Validations.h"
+
+@implementation NSNumber (Validations)
+
+@end
