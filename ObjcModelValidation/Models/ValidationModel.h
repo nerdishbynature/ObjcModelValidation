dfbad5b50140035101aa2805224333dc7917cfc1
Piet <piet@nerdishbynature.com>
2013-06-12 16:52:18 +0200

    [ADDED] validation model (new super class)

---
+++ ObjcModelValidation/Models/ValidationModel.h
@@ -0,0 +1,13 @@
+//
+//  ValidationModel.h
+//  ObjcModelValidation
+//
+//  Created by Piet Brauer on 12.06.13.
+//  Copyright (c) 2013 Piet Brauer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ValidationModel : NSObject
+
+@end
---
+++ ObjcModelValidation/Models/ValidationModel.m
@@ -0,0 +1,13 @@
+//
+//  ValidationModel.m
+//  ObjcModelValidation
+//
+//  Created by Piet Brauer on 12.06.13.
+//  Copyright (c) 2013 Piet Brauer. All rights reserved.
+//
+
+#import "ValidationModel.h"
+
+@implementation ValidationModel
+
+@end
