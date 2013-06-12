b866250c33a10dd6c6cc21ebf5b3b91f5e1a875c
Piet <piet@nerdishbynature.com>
2013-06-12 16:52:54 +0200

    [ADDED] stupid model class

---
+++ ObjcModelValidation/Models/SampleModel.h
@@ -0,0 +1,13 @@
+//
+//  SampleModel.h
+//  ObjcModelValidation
+//
+//  Created by Piet Brauer on 12.06.13.
+//  Copyright (c) 2013 Piet Brauer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SampleModel : NSObject
+
+@end
---
+++ ObjcModelValidation/Models/SampleModel.m
@@ -0,0 +1,15 @@
+//
+//  SampleModel.m
+//  ObjcModelValidation
+//
+//  Created by Piet Brauer on 12.06.13.
+//  Copyright (c) 2013 Piet Brauer. All rights reserved.
+//
+
+#import "SampleModel.h"
+
+@implementation SampleModel
+
+
+
+@end
