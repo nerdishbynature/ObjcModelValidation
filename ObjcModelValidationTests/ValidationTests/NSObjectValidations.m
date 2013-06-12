c6068f8d5b25eb21cd741236e0674b119f6b7901
Piet Brauer <piet@nerdishbynature.com>
2013-06-13 01:10:23 +0200

    [ADDED] presence tests for NSObject

---
+++ ObjcModelValidationTests/ValidationTests/NSObjectValidations.m
@@ -0,0 +1,32 @@
+//
+//  NSObjectValidations.m
+//  ObjcModelValidation
+//
+//  Created by Piet Brauer on 13.06.13.
+//  Copyright (c) 2013 Piet Brauer. All rights reserved.
+//
+
+#import "Kiwi.h"
+#import "NSObject+Validations.h"
+
+SPEC_BEGIN(NSObjectValidationTests)
+
+describe(@"Object presence", ^{
+    __block id obj;
+    
+    it(@"validates only with declaration", ^{
+        [[theValue([obj presence]) should] beFalse];
+    });
+    
+    it(@"validates with nil", ^{
+        obj = nil;
+        [[theValue([obj presence]) should] beFalse];
+    });
+    
+    it(@"validates with initialization", ^{
+        obj = @"Hello";
+        [[theValue([obj presence]) should] beTrue];
+    });
+});
+
+SPEC_END
