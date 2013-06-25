43b1de1158dfaf726e04f9b8d8a15b2495c8f329
Piet Brauer <piet@nerdishbynature.com>
2013-06-25 20:46:05 +0200

    [ADDED] name string validations

--- ObjcModelValidation/SampleModel.m
+++ ObjcModelValidation/SampleModel.m
@@ -18,4 +18,12 @@
     return [self errorWithLocalizedMessage:NSLocalizedString(@"You should be older than 18.", @"")];
 }
 
+-(NSError *)validateNameString:(NSString *)nameString{
+    if ([nameString lengthMin:4]) {
+        return nil;
+    }
+    
+    return [self errorWithLocalizedMessage:NSLocalizedString(@"Your name should be at least 4 characters long", @"")];
+}
+
 @end
